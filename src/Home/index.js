8e8f09759f3943e6d31ab917a24c987f62aebf53
Abdelrahman Mohamed <abdelrahmanmohamedsayed777@gmail.com>
2023-03-28 15:44:21 +0200

    done

--- src/Home/index.js
+++ src/Home/index.js
@@ -11,21 +11,21 @@ export default function Home({navigation}) {
 
     return (
     <Modules.View style={Modules.st.container}>
-      <Modules.ImageBackground
-        style={{ width: "100%", height: 230, marginBottom: 20 }}
-        source={Modules.headImage}
-      ></Modules.ImageBackground>
-
-      <Modules.Button 
-        title="Login"
-        style={{padding:10}}
-        onPress={() => navigation.navigate('Login')}          
-      />
-      <Modules.Button 
-        title="Singup"
-        style={{padding:10}}
-        onPress={() => navigation.navigate('Signup')}          
-      />
+      <Modules.ImageBackground style={{ width: "100%", height: 230, marginBottom: 20 }} source={Modules.headImage} ></Modules.ImageBackground>
+      <Modules.View style={{flexDirection: 'row', padding:10}}>
+        <Modules.Button 
+          title="Login"
+          style={{margin:10}}
+          onPress={() => navigation.navigate('Login')}          
+        />
+      </Modules.View>
+      <Modules.View style={{flexDirection: 'row', padding:10}}>
+        <Modules.Button 
+          title="Singup"
+          style={{margin:10}}
+          onPress={() => navigation.navigate('Signup')}          
+        />
+      </Modules.View>
     </Modules.View>);
 }
 
